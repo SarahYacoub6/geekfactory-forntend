557df68693a71fde5c7ca8d97c081ae4a2fd5254
sarahyacoub <sarahyacoub6@gmail.com>
2017-02-10 23:42:47 +0200

    updating

--- assignment/assignment11/task-manager.js
+++ assignment/assignment11/task-manager.js
@@ -1,22 +1,52 @@
-var taskfile=require('./task');
-var taskManager=function()
-{
+var Task=require('./task');
 
-}
-    ////////////////////////////////////
-taskManager.prototype.create=function()
+var create=function()
 {
-    return new manager();
-}
+    var task_arr = [];
+    let taskManager={
+       create:function(category,title,priority,estimate)
+       {
+           
+           task_arr.push(new Task(category,title,priority,estimate));
+           return task_arr[0];
+       } 
+       ,get:function(index)
+       {
+           return task_arr[index];
+       }
+       ,getAll:function()
+       {
+           return task_arr;
+       }
+       ,find:function(find_value){
+           var task_find_arr = [];
 
-var manager=function()
-{
-  
+           for(var i=0 ; i<task_arr.length ; i++)
+           {
+               if(task_arr[i].title.slice(0,20) === find_value)
+               {
+                    task_find_arr.push(task_arr[i]);
+               }
+               if(task_arr[i].category.slice(0,6) === find_value)
+               {
+                   task_find_arr.push(task_arr[i]);
+               }
+           }
+           return task_find_arr;
+       }
+        ,remove:function(index)
+       {
+           if( typeof(index)  === 'number'){
+               if(index < task_arr.length){
+                task_arr.pop(index);
+               }
+           }else{
+               task_arr.pop(task_arr.length -1)
+           }
+           
+       }
+   }
+   return taskManager;
 }
-  manager.prototype.create=function(category,title,priority,estimate)
-    {
-        return new Task(category,title,priority,estimate);
-    }
-module.exports=taskManager;
-module.exports=manager;
-module.exports=taskfile;
+   
+exports.create=create;
